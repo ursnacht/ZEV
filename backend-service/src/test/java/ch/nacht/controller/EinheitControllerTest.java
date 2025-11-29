c0d80f35463a87c28504d3640087d80cf7567a3c
Urs Nacht <urs.nacht@glue.ch>
2025-11-29 19:14:54 +0100

    Fix backend tests

--- backend-service/src/test/java/ch/nacht/controller/EinheitControllerTest.java
+++ backend-service/src/test/java/ch/nacht/controller/EinheitControllerTest.java
@@ -6,6 +6,7 @@ import ch.nacht.service.EinheitService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(EinheitController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class EinheitControllerTest {
 
     @Autowired
